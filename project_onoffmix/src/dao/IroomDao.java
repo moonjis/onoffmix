219c1da625bce83f1e96f874084052c26d98634c
5CLASS-184 <5CLASS-184@HB01-17>
2017-07-18 11:10:57 +0900

    20170718 1110

--- project_onoffmix/src/dao/IroomDao.java
+++ project_onoffmix/src/dao/IroomDao.java
@@ -3,23 +3,39 @@ package dao;
 import java.util.HashMap;
 import java.util.List;
 
-
 import model.Category;
 import model.Room;
 
 public interface IroomDao {
-	public List<Room> selectAll();
+	
+	public List<Room> selectAll(HashMap<String, Object> params);
+
 	public List<Category> selectCategory();
+
 	public Room selectOne(int num);
+
 	public void insertRoom(Room room);
+
 	public void updateRoom(Room room);
+
 	public void deleteRoom(int num);
+
 	public void deleteAttach(int num);
+
 	public void updateCount(Room room);
+
 	public void joinRoom(HashMap<String, Object> params);
+
 	public void outRoom(int num);
+
 	public void insertAttach(HashMap<String, Object> params);
+
 	public int getCount();
+
 	public List<Room> selectMyRooms(HashMap<String, Object> map);
-	public int getCntAllMyRooms(String id);	
+
+	public int getCntAllMyRooms(String id);
+
+	public int roomTotalCount();
+	
 }
--- project_onoffmix/src/service/RoomService.java
+++ project_onoffmix/src/service/RoomService.java
@@ -10,18 +10,24 @@ import org.springframework.web.multipart.MultipartFile;
 import model.Category;
 import model.Room;
 
-
 public interface RoomService {
-	public List<Room> getRoomList();
+	
+	public HashMap<String, Object> getRoomList(int page);
+
 	public List<Category> getCategoryList();
+
 	public Room readRoom(int num);
+
 	public boolean createRoom(HttpServletRequest req, Room room, MultipartFile file);
-	public HashMap<String,Object> selectMyRooms(HashMap<String, Object> map);
+
+	public HashMap<String, Object> selectMyRooms(HashMap<String, Object> map);
+
 	public boolean joinRoom(String id, Room room);
+
 	public boolean deleteRoom(int num);
+
 	public boolean updateRoom(Room room);
+
 	public boolean outRoom(int num);
-
-
 
 }
